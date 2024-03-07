ed4c4be1faeaabdab568d697b9685ad5b3116052
Glenn Hiemstra <glenn.hiemstra@gmail.com>
2024-03-07 19:22:40 +0100

    add stringr fix

--- R/install_dependencies.R
+++ R/install_dependencies.R
@@ -6,5 +6,9 @@
 
   # Install the specific version from a URL
   remotes::install_url("https://raw.githubusercontent.com/Glender/DutchSentimentAnalysis/main/inst/script/vwr_0.3.0.tar.gz")
+
+  if(packageVersion("stringr") != "1.4.0"){
+    devtools::install_version("stringr", version = "1.4.0", repos = "http://cran.us.r-project.org")
+  }
 }
 
